bb426c5eb0b2432dde086d2c8084e03544c8615f
Eteran07 <edgar.teran@correo.unimet.edu.ve>
2024-11-11 20:04:41 -0400

    Se realizaron cambios en el main como version inicial para probar la EDD Tree y Nodo

--- src/EstructuraDeDatos/Main.java
+++ src/EstructuraDeDatos/Main.java
@@ -14,7 +14,24 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       Tree tree = new Tree();
+        
+        tree.insert(8, -1, -1, null);
+        tree.insert(10, 0, 0,tree.getRoot());
+        tree.insert(22, 0,  0,tree.getRoot());
+        tree.insert(5, 0,  0,tree.getRoot());
+        tree.insert(80, 0, 0, tree.getRoot());
+        tree.insert(15, 0, 0, tree.getRoot());
+        tree.insert(3, 0, 0, tree.getRoot());
+        tree.insert(1, 3,2, tree.getRoot());
+        tree.insert(7, 3,5, tree.getRoot());
+        tree.insert(9, 6,2, tree.getRoot());
+        tree.insert(99, 9,16, tree.getRoot());
+
+
+        tree.print(tree.getRoot());
+        tree.printAncestors(32);
     }
+    
     
 }
